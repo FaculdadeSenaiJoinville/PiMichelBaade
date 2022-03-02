944f4f785401606501d801662c9ba861fbf1e2b9
mchbaade <michel_baade@estudante.sc.senai.br>
2022-03-02 19:48:23 -0300

    Corrigindo os problemas de duplicidades do DOM
    Corrigindo os problemas de duplicidades do DOM

--- ProjetoIndividual/WebContent/js/usuario.js
+++ ProjetoIndividual/WebContent/js/usuario.js
@@ -223,17 +223,17 @@ $(document).ready(function() {
 			data: "login="+login,
 			success: function(usuario){
 				
-				document.frmEditaUsuario.login.value = usuario.login;
+				document.frmEditaUsuario.login2.value = usuario.login;
 				
 				//usuario.senha = btoa(document.frmEditaUsuario.senha.value);
 	
 				//document.frmEditaUsuario.senha.value = usuario.senha;
-				document.frmEditaUsuario.nivel_usuario.value = usuario.nivel_usuario;
-				document.frmEditaUsuario.datanasc.value = usuario.data_nasc;
-				document.frmEditaUsuario.cpf.value = usuario.cpf;
-				document.frmEditaUsuario.nome.value = usuario.nome;
-				document.frmEditaUsuario.email.value = usuario.email;
-				document.frmEditaUsuario.telefone.value = usuario.telefone;		
+				document.frmEditaUsuario.nivel_usuario2.value = usuario.nivel_usuario;
+				document.frmEditaUsuario.datanasc2.value = usuario.data_nasc;
+				document.frmEditaUsuario.cpf2.value = usuario.cpf;
+				document.frmEditaUsuario.nome2.value = usuario.nome;
+				document.frmEditaUsuario.email2.value = usuario.email;
+				document.frmEditaUsuario.telefone2.value = usuario.telefone;		
 				
 				
 				if(usuario.cpf == "0"){
@@ -246,7 +246,44 @@ $(document).ready(function() {
 					width: 1200,
 					modal: true,
 					buttons:{
-						"Salvar": function(){					
+						"Salvar": function(){
+							
+							if(document.frmEditaUsuario.nome2.value == ""){
+								SONHOS.exibirAviso("O campo nome foi preenchido incorretamente!");
+								return false;
+							}
+							
+							var cpf = document.frmEditaUsuario.cpf2.value;
+							var expRegCpf = new RegExp("([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})");
+							if(!expRegCpf.test(cpf)){
+								SONHOS.exibirAviso("O CPF foi preenchido incorretamente! Por favor preencha sem pontuação, conforme o seguinte exemplo: 09484433363");
+								return false;
+							}
+							if(document.frmEditaUsuario.datanasc2.value == ""){
+								SONHOS.exibirAviso("A data de nascimento foi preenchida incorretamente!");
+								return false;
+							}
+							
+							var telefone = document.frmEditaUsuario.telefone2.value;
+							var expRegTel = new RegExp("([0-9]{3}|[0-9]{2})?([0-9]{2})([0-9]{4,5})([0-9]{4})");
+							if(!expRegTel.test(telefone)){
+								SONHOS.exibirAviso("O telefone foi preenchido incorretamente! Por favor preencha sem pontuação, conforme um dos seguintes exemplos: \n 5547996015808 \n 47996015808");
+								return false;
+							}
+							if(document.frmEditaUsuario.email2.value == ""){
+								SONHOS.exibirAviso("O e-mail foi preenchido incorretamente!");
+								return false;
+							}
+							
+							if(document.frmEditaUsuario.login2.value == ""){
+								SONHOS.exibirAviso("O login foi preenchido incorretamente!");
+								return false;
+							}
+							if(document.frmEditaUsuario.nivel_usuario2.value == ""){
+								SONHOS.exibirAviso("O nível de usuário foi preenchido incorretamente!");
+								return false;
+							}
+								
 						SONHOS.usuario.editar();
 											
 						},
@@ -274,20 +311,20 @@ $(document).ready(function() {
 	SONHOS.usuario.editar = function(){
 		
 		var usuario = new Object();
-		usuario.login = document.frmEditaUsuario.login.value;
+		usuario.login = document.frmEditaUsuario.login2.value;
 		//usuario.senha = document.frmEditaUsuario.senha.value;
 		
 		
-		usuario.senha = btoa(document.frmEditaUsuario.senha.value);
+		usuario.senha = btoa(document.frmEditaUsuario.senha2.value);
 	
 		
 		
-		usuario.nivel_usuario = document.frmEditaUsuario.nivel_usuario.value;
-		usuario.data_nasc = document.frmEditaUsuario.datanasc.value;
-		usuario.cpf = document.frmEditaUsuario.cpf.value;
-		usuario.nome = document.frmEditaUsuario.nome.value;
-		usuario.email = document.frmEditaUsuario.email.value;
-		usuario.telefone = document.frmEditaUsuario.telefone.value;
+		usuario.nivel_usuario = document.frmEditaUsuario.nivel_usuario2.value;
+		usuario.data_nasc = document.frmEditaUsuario.datanasc2.value;
+		usuario.cpf = document.frmEditaUsuario.cpf2.value;
+		usuario.nome = document.frmEditaUsuario.nome2.value;
+		usuario.email = document.frmEditaUsuario.email2.value;
+		usuario.telefone = document.frmEditaUsuario.telefone2.value;
 		
 		$.ajax({
 			type:"PUT",
