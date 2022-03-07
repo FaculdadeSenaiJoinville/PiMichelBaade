fa45c740a8e4358d2256cfbf85f2a9cef0703b4d
mchbaade <michel_baade@estudante.sc.senai.br>
2022-03-07 19:34:50 -0300

    Ajustando formatações
    Ajustando formatações

--- ProjetoIndividual/WebContent/js/usuario.js
+++ ProjetoIndividual/WebContent/js/usuario.js
@@ -1,12 +1,10 @@
 SONHOS.usuario = new Object();
 
 function validaUsuario(){
-	
 	if(document.frmAddUsuario.nome.value == ""){
 		SONHOS.exibirAviso("O campo nome foi preenchido incorretamente!");
 		return false;
 	}
-	
 	var cpf = document.frmAddUsuario.cpf.value;
 	var expRegCpf = new RegExp("([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})");
 	if(!expRegCpf.test(cpf)){
@@ -17,7 +15,6 @@ function validaUsuario(){
 		SONHOS.exibirAviso("A data de nascimento foi preenchida incorretamente!");
 		return false;
 	}
-	
 	var telefone = document.frmAddUsuario.telefone.value;
 	var expRegTel = new RegExp("([0-9]{3}|[0-9]{2})?([0-9]{2})([0-9]{4,5})([0-9]{4})");
 	if(!expRegTel.test(telefone)){
@@ -28,7 +25,6 @@ function validaUsuario(){
 		SONHOS.exibirAviso("O e-mail foi preenchido incorretamente!");
 		return false;
 	}
-	
 	if(document.frmAddUsuario.login.value == ""){
 		SONHOS.exibirAviso("O login foi preenchido incorretamente!");
 		return false;
@@ -41,35 +37,26 @@ function validaUsuario(){
 		SONHOS.exibirAviso("O nível de usuário foi preenchido incorretamente!");
 		return false;
 	}
-	
-	
 	SONHOS.usuario.cadastrar()
 }
-
 $(document).ready(function() {
 
 	SONHOS.usuario = function(){};
 
-	
 	SONHOS.usuario.validaUsuarioAlterar = function(){
-		
 		console.log(document.frmEditaUsuario.nome2.value);
 		if (document.frmEditaUsuario.nome2.value == "") {
 	        SONHOS.exibirAviso("O campo nome foi preenchido incorretamente!");
-	       
 	    }
-
 	    var cpf = document.frmEditaUsuario.cpf2.value;
 	    var expRegCpf = new RegExp("([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})");
 	    if (!expRegCpf.test(cpf)) {
 	        SONHOS.exibirAviso("O CPF foi preenchido incorretamente! Por favor preencha sem pontuação, conforme o seguinte exemplo: 09484433363");
-	   
 	    }
 	    if (document.frmEditaUsuario.datanasc2.value == "") {
 	        SONHOS.exibirAviso("A data de nascimento foi preenchida incorretamente!");
 	        return false;
 	    }
-
 	    var telefone = document.frmEditaUsuario.telefone2.value;
 	    var expRegTel = new RegExp("([0-9]{3}|[0-9]{2})?([0-9]{2})([0-9]{4,5})([0-9]{4})");
 	    if (!expRegTel.test(telefone)) {
@@ -80,7 +67,6 @@ $(document).ready(function() {
 	        SONHOS.exibirAviso("O e-mail foi preenchido incorretamente!");
 	        return false;
 	    }
-
 	    if (document.frmEditaUsuario.login2.value == "") {
 	        SONHOS.exibirAviso("O login foi preenchido incorretamente!");
 	        return false;
@@ -92,75 +78,59 @@ $(document).ready(function() {
 	    return true;
 	}
 	
-	
 	SONHOS.usuario.cadastrar = function(){
-		
 		var usuario = new Object();
 		usuario.login = document.frmAddUsuario.login.value;
 		usuario.senha = btoa(document.frmAddUsuario.senha.value);
-	
 		document.frmAddUsuario.senha.value = usuario.senha;
-		
 		usuario.nivel_usuario = document.frmAddUsuario.nivel_usuario.value;
 		usuario.data_nasc = document.frmAddUsuario.datanasc.value;
 		usuario.cpf = document.frmAddUsuario.cpf.value;
-		usuario.nome = document.frmAddUsuario.nome.value;	
+		usuario.nome = document.frmAddUsuario.nome.value;
 		usuario.email = document.frmAddUsuario.email.value;
 		usuario.telefone = document.frmAddUsuario.telefone.value;
 		
 			$.ajax({
-				
 				type: "POST",
 				url: SONHOS.PATH+"usuario/inserir",
 				data:JSON.stringify(usuario),
-				success: function(msg){								
+				success: function(msg){
 					SONHOS.exibirAviso(msg);
 					$("#addUsuario").trigger("reset");
 					SONHOS.usuario.buscar();
 				},
 				error: function (info){
 					SONHOS.exibirAviso("Erro ao cadastrar um novo usuario: "+ info.status +" - "+ info.statusText);
-				}		
+				}
 			});
-		
-		
 	}
 	
-	
 	SONHOS.usuario.buscar = function(){
-	
 	var valorBusca = $("#campoBuscaUsuario").val();
-	
 	$.ajax({
-				
 				type: "GET",
 				url: SONHOS.PATH+"usuario/buscar",
 				data:"valorBusca="+valorBusca,
 				success: function(dados){
-					
-					dados = JSON.parse(dados);					
-					$("#listaUsuarios").html(SONHOS.usuario.exibir(dados));	
+					dados = JSON.parse(dados);
+					$("#listaUsuarios").html(SONHOS.usuario.exibir(dados));
 				},
 				error: function (info){
 					SONHOS.exibirAviso("Erro ao consultar os funcionários: "+ info.status +" - "+ info.statusText);
-				}		
+				}
 			});
 	};
 	
-	
 	SONHOS.usuario.exibir = function (listaDeUsuarios){
-		
 		var tabela = "<table>"+
 		"<tr>"+
 		"<th>Nível de funcionário</th>"+
 		"<th>Nome</th>"+
 		"<th>Login</th>"+
-		"<th>E-mail</th>"+		
+		"<th>E-mail</th>"+
 		"<th class='acoes'>Ações</th>"+
 		"</tr>";
-		
 		if(listaDeUsuarios != undefined && listaDeUsuarios.length > 0){
-			
 			for(var i=0;i<listaDeUsuarios.length; i++){
 				tabela += "<tr>"+
 				"<td>"+listaDeUsuarios[i].nivel_usuario+"</td>"+
@@ -172,10 +142,7 @@ $(document).ready(function() {
 					"<a onclick=\"SONHOS.usuario.exibirExclusao('"+listaDeUsuarios[i].login+"')\"><img src='/ProjetoIndividual/imgs/delete.png' alt='Excluir registro'></a>"+
 				"</td>"+
 				"</tr>"
-					
 			}
-			
-			
 		}else if(listaDeUsuarios == ""){
 			tabela += "<tr><td colspan='5'>Nenhum registro encontrado</td></tr>";
 		}
@@ -183,31 +150,24 @@ $(document).ready(function() {
 		return tabela;
 	};
 	
-	
 	SONHOS.usuario.buscar();
 	
-	
-	
 	SONHOS.usuario.excluir = function(login){
-		
 		$.ajax({
 			type:"DELETE",
 			url: SONHOS.PATH + "usuario/excluir/"+login,
 			success: function(msg){
 				SONHOS.usuario.buscar();
-				SONHOS.exibirAviso(msg);				
-				$("#modalExcluiUsuario").dialog("close");			
+				SONHOS.exibirAviso(msg);
+				$("#modalExcluiUsuario").dialog("close");		
 			},
 			error: function(info){
 				SONHOS.exibirAviso("Erro ao excluir o funcionário: "+ info.status +" - "+ info.statusText);
 			}
-		});		
+		});
 	};
 	
-	
-	
 	SONHOS.usuario.exibirExclusao = function(login){
-		
 		$.ajax({
 			type:"GET",
 			url: SONHOS.PATH + "usuario/buscarPorLogin",
@@ -215,13 +175,9 @@ $(document).ready(function() {
 			success: function(usuario){
 				var teste = usuario.login;
 				document.frmExcluiUsuario.login.value = usuario.login;
-				
-				
 				if(usuario.login == undefined){
 					SONHOS.exibirAviso("O funcionário não foi encontrado no banco de dados. Por favor recarregue a página e tente novamente!");
-				}else{
-				
-				
+				}else{				
 				var modalExcluiUsuario = {
 					title: "Excluir funcionário",
 					height: 400,
@@ -238,56 +194,37 @@ $(document).ready(function() {
 					close: function(){
 					}
 				};
-				
 				$("#modalExcluiUsuario").dialog(modalExcluiUsuario);
-					
 				}
-					
 			},
 			error: function(info){
 				SONHOS.exibirAviso("Erro ao buscar o funcionário para exclusão: "+ info.status +" - "+ info.statusText);
 			}
-		});		
+		});
 	};
 	
-	
 	SONHOS.usuario.exibirEdicao = function(login){
-		
-		
-		
-		
-		
-		
-		
 		$.ajax({
 			type:"GET",
 			url: SONHOS.PATH + "usuario/buscarPorLogin",
 			data: "login="+login,
 			success: function(usuario){
-				
 				document.frmEditaUsuario.login2.value = usuario.login;
-				
-				//usuario.senha = btoa(document.frmEditaUsuario.senha.value);
-	
-				//document.frmEditaUsuario.senha.value = usuario.senha;
 				document.frmEditaUsuario.nivel_usuario2.value = usuario.nivel_usuario;
 				document.frmEditaUsuario.datanasc2.value = usuario.data_nasc;
 				document.frmEditaUsuario.cpf2.value = usuario.cpf;
 				document.frmEditaUsuario.nome2.value = usuario.nome;
 				document.frmEditaUsuario.email2.value = usuario.email;
-				document.frmEditaUsuario.telefone2.value = usuario.telefone;		
-				
+				document.frmEditaUsuario.telefone2.value = usuario.telefone;	
 				
 				function leftPad(value, totalWidth, paddingChar) {
 					  var length = totalWidth - value.toString().length + 1;
 					  document.frmEditaUsuario.cpf2.value = Array(length).join(paddingChar || '0') + value;
 					};
 				leftPad(document.frmEditaUsuario.cpf2.value, 11);
-				
 				if(usuario.cpf == "0"){
 				SONHOS.exibirAviso("O funcionário não foi encontrado no banco de dados. Por favor recarregue a página e tente novamente!");
 				}else{
-				
 				var modalEditaUsuario = {
 					title: "Editar funcionário",
 					height: 400,
@@ -295,11 +232,9 @@ $(document).ready(function() {
 					modal: true,
 					buttons:{
 						"Salvar": function(){
-							
-						if(SONHOS.usuario.validaUsuarioAlterar()){
-							SONHOS.usuario.editar();
-						}
-											
+							if(SONHOS.usuario.validaUsuarioAlterar()){
+								SONHOS.usuario.editar();
+							}
 						},
 						"Cancelar": function(){
 							$(this).dialog("close");
@@ -308,38 +243,26 @@ $(document).ready(function() {
 					close: function(){
 					}
 				};
-				
 				$("#modalEditaUsuario").dialog(modalEditaUsuario);
-	
-				
 				}
-		
 			},
 			error: function(info){
 				SONHOS.exibirAviso("Erro ao buscar o funcionário para edição: "+ info.status +" - "+ info.statusText);
 			}
-		});		
+		});
 	};
 	
-	
 	SONHOS.usuario.editar = function(){
-		
 		var usuario = new Object();
 		usuario.login = document.frmEditaUsuario.login2.value;
-		//usuario.senha = document.frmEditaUsuario.senha.value;
-		
-		
 		usuario.senha = btoa(document.frmEditaUsuario.senha2.value);
-	
-		
-		
 		usuario.nivel_usuario = document.frmEditaUsuario.nivel_usuario2.value;
 		usuario.data_nasc = document.frmEditaUsuario.datanasc2.value;
 		usuario.cpf = document.frmEditaUsuario.cpf2.value;
 		usuario.nome = document.frmEditaUsuario.nome2.value;
 		usuario.email = document.frmEditaUsuario.email2.value;
 		usuario.telefone = document.frmEditaUsuario.telefone2.value;
-		
+	
 		$.ajax({
 			type:"PUT",
 			url: SONHOS.PATH + "usuario/alterar",
@@ -352,9 +275,6 @@ $(document).ready(function() {
 			error: function(info){
 				SONHOS.exibirAviso("Erro ao editar o funcionário: "+ info.status +" - "+ info.statusText);
 			}
-		});			
-	};		
+		});
+	};
 });
-
-
-
--- ProjetoIndividual/WebContent/js/venda.js
+++ ProjetoIndividual/WebContent/js/venda.js
@@ -19,140 +19,97 @@ $(document).ready(function() {
 	var valorTotalAntigo = parseFloat(0);
 	
 	SONHOS.venda.guardaValores = function(id){
-		
 		var produtoAtual = document.getElementsByName('selProduto[]');
 		var categoriaAtual = document.getElementsByName('selCategoria[]');
 		var QuantidadePesoAtual = document.getElementsByName('QuantidadePeso[]');
 		var valorAtual = document.getElementsByName('valor[]');
-			
 		for (var i = 0; i < $("tr.detalhes").length; i++) {
 			produtoAntigo[i] = produtoAtual[i].value;
 			categoriaAntigo[i] = categoriaAtual[i].value;
 			QuantidadePesoAntigo[i] = QuantidadePesoAtual[i].value;
 			valorAntigo[i] = valorAtual[i].value;
-		}	
-		
+		}
 		$("#totalValores").html("Total: R$ "+SONHOS.formatarDinheiro(valorTotalAntigo));
 	}
 
-
 	SONHOS.venda.carregarCategorias = function(id){
-		
 		var camposCategorias = document.getElementsByName('selCategoria[]');
-		
 			$.ajax({
 				type: "GET",
 				url: SONHOS.PATH + "categoria/buscar",
-				success: function (categorias) {				
+				success: function (categorias) {
 					$(camposCategorias[id]).html("");
-					
 					if (categorias.length){
-						
 						var option = document.createElement("option");
 						option.setAttribute ("value", "");
-						option.innerHTML = ("Escolha");					
+						option.innerHTML = ("Escolha");
 						$(camposCategorias[id]).append(option);
-						
 						for(var i = 0; i< categorias.length; i++){
-							
 							var option = document.createElement("option");
 							option.setAttribute ("value", categorias[i].id);
 							option.innerHTML = (categorias[i].nome);
 							$(camposCategorias[id]).append(option);
-										
-						}
-						
+						}	
 					}else{
-						
 						var option = document.createElement("option");
 						option.setAttribute ("value", "");
 						option.innerHTML = ("Cadastre uma categoria primeiro!");
-						
 						$(camposCategorias[id]).addClass("aviso");
 						$(camposCategorias[id]).append(option);
-						
 					}
-					
-					
 				},
 				error: function (info) {
-				
 				SONHOS.exibirAviso("Erro ao buscar as categorias: "+ info.status + " - " +info.statusText);
-				
 				$(camposCategorias[id]).html("");
 				var option = document.createElement("option");
 				option.setAttribute ("value", "");
 				option.innerHTML = ("Erro ao carregar categorias!");
 				$(camposCategorias[id]).addClass("aviso");
 				$(camposCategorias[id]).append(option);
-				
 				}
-			});		
+			});
 		}
-
 		SONHOS.venda.carregarCategorias(0);
-	
-	//Criação de uma nova linha de produto na venda
-	
-	
 	$(".botaoAdd").click(function (){
-		
 		var novoCampo = $("tr.detalhes:first").clone();
-		//ao setar os novos campos do clone ele não deixa alterar mais por javascript.
 		novoCampo.find("input").val(0);
 		novoCampo.insertAfter("tr.detalhes:last");
 		SONHOS.venda.guardaValores();
-		
 	});	
 	
 	SONHOS.venda.removeCampo = function(botao) {
-		
 		if($("tr.detalhes").length > 1){
-			
 			$(botao).parent().parent().remove();
 			SONHOS.venda.guardaValores();
-			SONHOS.venda.AtualizaValor();			
+			SONHOS.venda.AtualizaValor();
 		}else{
 			SONHOS.exibirAviso("A última linha não pode ser removida!");
 		}
 	}
 	
-	
 	SONHOS.venda.carregarProdutos = function(botao) {
-		
 		var selectProduto = document.getElementsByName('selProduto[]');
 		var categoriaAtual =  document.getElementsByName('selCategoria[]');
-		
 		for (var j = 0; j < selectProduto.length; j++) {
-			
-			if ((categoriaAntigo[j] != categoriaAtual[j].value)) {
-				
+			if ((categoriaAntigo[j] != categoriaAtual[j].value)) {	
 				linhaAlterada = j;
 			}
-			
 		}
 		if ((categoriaAtual[linhaAlterada].value=="")) {
-			
 			SONHOS.venda.guardaValores();
-			
 			return false;
 		}
 		categoriaCod = categoriaAtual[linhaAlterada].value;
 		$(selectProduto[linhaAlterada]).html("<option>Aguarde</option>");
-		
-		
 		$.ajax({
 			type: "GET",
 			url: SONHOS.PATH + "produto/buscarParaVenda",
 			data: "categoria="+categoriaCod,
 			success: function (produtos) {
-				
 				produtos = JSON.parse(produtos);
 				$(selectProduto[linhaAlterada]).html("");
-				
 				if (produtos.length) {
 					$(selectProduto[linhaAlterada]).removeClass("aviso");
-					
 					var option = document.createElement("option");
 					option.setAttribute("value","");
 					option.innerHTML = ("Escolha");
@@ -164,79 +121,57 @@ $(document).ready(function() {
 						$(selectProduto[linhaAlterada]).append(option);
 					}
 				}else{
-					
 					var option = document.createElement("option");
 					option.setAttribute("value","");
 					option.innerHTML = ("Não há produtos correspondentes!");
 					$(selectProduto[linhaAlterada]).append(option);
-				
 					$(selectProduto[linhaAlterada]).addClass("aviso");
-					
 				}
-				
 			},
 			error: function (info) {
-			
-				
 				SONHOS.exibirAviso("Erro ao buscar os produtos: " + info.status + " - " + info.statusText);
-				
 				$(selectProduto[linhaAlterada]).html("");
 				var option = document.createElement("option");
 				option.setAttribute("value","");
 				option.innerHTML = ("Erro ao carregar produtos!");
 				$(selectProduto[linhaAlterada]).append(option);
 				$(selectProduto[linhaAlterada]).addClass("aviso");
-				
 			}
 		});
-		
-		SONHOS.venda.guardaValores();	
+		SONHOS.venda.guardaValores();
 	}
 	
 	SONHOS.venda.validaDetalhe = function() {
-	
 	var categoriasValidar = document.getElementsByName('selCategoria[]');
 	var produtosValidar = document.getElementsByName('selProduto[]');
-	
 	var qtdeValidar = document.getElementsByName('QuantidadePeso[]');
 	var valorValidar = document.getElementsByName('valor[]');
-	
 	for (var i = 0; i < produtosValidar.length; i++) {
-		
 		var linha = i+1;
 		if((qtdeValidar[i].value<="0")){
 			SONHOS.exibirAviso("Não é permitido inserir um produto com quantidade negativa(linha "+linha+").");
 			return false;
 		}
 		if ((produtosValidar[i].value=="")||(categoriasValidar[i].value=="")||(qtdeValidar[i].value=="")||(valorValidar[i].value=="")) {
-			
 			SONHOS.exibirAviso("A linha "+linha+" não foi completamente preenchida.");
 			return false;
-			
 		}
 	}
 	return true;
-		
 	}
-	
-	
+
 	SONHOS.venda.cadastrar = function() {
-	
 		if (SONHOS.venda.validaDetalhe()) {
-		
 			var venda = new Object();
-			
 			var produtos = document.getElementsByName('selProduto[]');
 			var quantidades = document.getElementsByName('QuantidadePeso[]');
 			var valores = document.getElementsByName('valor[]');
 			venda.produtos = new Array(produtos.length);
-			
 			for (var i = 0; i < produtos.length; i++) {
 				venda.produtos[i] = new Object();
 				venda.produtos[i].idProduto = produtos[i].value;
 				venda.produtos[i].quantidade = quantidades[i].value;
 				venda.produtos[i].valor = valores[i].value;
-				
 			}
 			
 			$.ajax({
@@ -252,38 +187,25 @@ $(document).ready(function() {
 					SONHOS.exibirAviso(msg);
 				},
 				error: function (info) {
-				
 					SONHOS.exibirAviso("Erro ao cadastrar um novo produto: "+info.status+" - "+info.statusText);
 				}
 			});
-			
 		}
-		
 	}
 	
-	
 SONHOS.venda.AtualizaValor = function() {
-		
 	var linhaAlterada = "0";
-	
 	var selectProduto = document.getElementsByName('selProduto[]');
-	
 	for (var j = 0; j < selectProduto.length; j++) {
-		
 		if ((produtoAntigo[j] != selectProduto[j].value)) {
 			linhaAlterada = j;
 		}
-		
 	}
-	
 	var QuantidadePesoAtual = document.getElementsByName('QuantidadePeso[]');
-	
 	for (var j = 0; j < QuantidadePesoAtual.length; j++) {
-		
 		if ((QuantidadePesoAntigo[j] != QuantidadePesoAtual[j].value)) {
 			linhaAlterada = j;
 		}
-		
 	}
 		$.ajax({
 			type: "POST",
@@ -291,9 +213,7 @@ SONHOS.venda.AtualizaValor = function() {
 			data:"produtoAntigo="+selectProduto[linhaAlterada].value,
 			async:false,
 			success: function (valoresProdutos) {
-				
 				valoresProdutos = JSON.parse(valoresProdutos);
-				
 				var venda = new Object();
 				var quantidades = document.getElementsByName('QuantidadePeso[]');
 				if(quantidades[linhaAlterada].value.indexOf(",") > -1){
@@ -302,19 +222,16 @@ SONHOS.venda.AtualizaValor = function() {
 				}
 				var valores = document.getElementsByName('valor[]');
 				var idprodutos = document.getElementsByName('selProduto[]');
-				
 				venda.produtos = new Array(valoresProdutos.length);
 				valores[linhaAlterada].value = (valoresProdutos * quantidades[linhaAlterada].value);
-				
 				valorTotalAntigo = parseFloat(0);
 				for (var i = 0; i < valores.length; i++) {
-					valorTotalAntigo = parseFloat(valorTotalAntigo) + parseFloat(valores[i].value); 
+					valorTotalAntigo = parseFloat(valorTotalAntigo) + parseFloat(valores[i].value);
 				}
 				SONHOS.venda.guardaValores();
 			},
 			error: function (info) {
-						
-				SONHOS.exibirAviso("Erro ao buscar os produtos: " + info.status + " - " + info.statusText);			
+				SONHOS.exibirAviso("Erro ao buscar os produtos: " + info.status + " - " + info.statusText);
 				$(selectProduto[linhaAlterada]).html("");
 				var option = document.createElement("option");
 				option.setAttribute("value","");
@@ -324,8 +241,6 @@ SONHOS.venda.AtualizaValor = function() {
 				SONHOS.venda.guardaValores();
 			}
 		});
-		
 		SONHOS.venda.guardaValores();
 	}
-	
 });
